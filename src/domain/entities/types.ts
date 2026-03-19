d7928818a92086c437bc716e53dec16dfb766fee
ivan.bomko <vanya.bomko@gmail.com>
2026-03-19 10:49:55 +0100

    [initial]: all types added

--- src/domain/entities/types.ts
+++ src/domain/entities/types.ts
@@ -1,1 +1,6 @@
 export type StudentId = string & { readonly __brand: "StudentId" };
+export type CourseCode = string & { readonly __brand: "CourseCode" };
+export type Email = string & { readonly __brand: "Email" };
+export type Credits = string & { readonly __brand: "Credits" };
+export type Semester = string & { readonly __brand: "Semester" };
+export type EnrollmentId = string & { readonly __brand: "EnrollmentId" };
