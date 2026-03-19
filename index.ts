6be0e114b898ad140bd510b1dcba311bc19b6903
Charles Stucke <charles-otto.stucke@epita.fr>
2026-03-19 14:48:01 +0100

    feat: Implemented Student and Enrollment tests for all attributes

--- index.ts
+++ index.ts
@@ -1,4 +1,114 @@
-import { v4 as uuidv4 } from "uuid"
+import { Student, createStudent } from "./src/domain/entities/studentEntity";
+import {
+  Enrollment,
+  createEnrollment,
+} from "./src/domain/entities/enrollmentEntity";
+import { Course, createCourse } from "./src/domain/entities/studentEntity";
 
-const id = uuidv4()
-console.log("running the application ... ", id)
+import { v4 as uuidv4 } from "uuid";
+
+const id = uuidv4();
+console.log("running the application ... ", id);
+
+// Student Tests
+try {
+  const student0 = createStudent(
+    "STU930842",
+    "Job Bohnson",
+    "jobbohnson@gmail.com",
+    4,
+  );
+} catch (error) {
+  console.log(error);
+}
+
+try {
+  const student1 = createStudent(
+    "-1",
+    "Bob Johnson",
+    "bobjohnson@gmail.com",
+    1,
+  );
+} catch (error) {
+  console.log(error);
+}
+
+try {
+  const student2 = createStudent(
+    "STU123456",
+    "Bob Johnson",
+    "bobjohnsongmail.com",
+    1,
+  );
+} catch (error) {
+  console.log(error);
+}
+
+try {
+  const student3 = createStudent(
+    "STU123456",
+    "Bob Johnson",
+    "bobjohnson@gmail.com",
+    500,
+  );
+} catch (error) {
+  console.log(error);
+}
+
+// Course Tests
+
+// Enrollment Tests
+try {
+  const enrollment0 = createEnrollment(
+    "ENR000000",
+    "STU930842",
+    "CS101",
+    "Fall 2026",
+  );
+} catch (error) {
+  console.log(error);
+}
+
+try {
+  const enrollment1 = createEnrollment(
+    "ERN000000",
+    "STU930842",
+    "CS101",
+    "Fall 2026",
+  );
+} catch (error) {
+  console.log(error);
+}
+
+try {
+  const enrollment2 = createEnrollment(
+    "ENR000000",
+    "STU9",
+    "CS101",
+    "Fall 2026",
+  );
+} catch (error) {
+  console.log(error);
+}
+
+try {
+  const enrollment3 = createEnrollment(
+    "ENR000000",
+    "STU930842",
+    "F3",
+    "Fall 2026",
+  );
+} catch (error) {
+  console.log(error);
+}
+
+try {
+  const enrollment4 = createEnrollment(
+    "ENR000000",
+    "STU930842",
+    "CS101",
+    "Splinter 6",
+  );
+} catch (error) {
+  console.log(error);
+}
