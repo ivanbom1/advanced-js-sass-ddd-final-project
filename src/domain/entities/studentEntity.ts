27d78d95f64a52bb9641b0fb1d531c7577934ef5
ivan.bomko <vanya.bomko@gmail.com>
2026-03-19 11:37:54 +0100

    [entity]: student entity added

---
+++ src/domain/entities/studentEntity.ts
@@ -0,0 +1,26 @@
+import { StudentId, Email, Credits, createStudentId, createEmail  } from "./types";
+
+export type Student = {
+    id: StudentId,
+    name: string,
+    email: Email,
+    enrolledCredits?: number;
+};
+
+export function createStudent(
+    id: StudentId,
+    name: string,
+    email: Email,
+    enrolledCredits?: number,
+): Student {
+    const validId = createStudentId(id);
+    const validEmail = createEmail(email);
+    if (!name || name.trim() === "") {
+        throw new Error("Student's name can't be empty");
+    }
+
+    if (enrolledCredits && enrolledCredits > 18) {
+        throw new Error("Max 18 credits per semester");
+    }
+    return {id: validId, name, email: validEmail, enrolledCredits};
+}
